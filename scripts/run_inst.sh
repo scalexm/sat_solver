c17e1bd4376b7b731f04613395c94f79f7d0010f
nicolaspierrelevy <nicolaspierrelevy@gmail.com>
2016-03-20 11:48:59 +0100

    Fix scripts portability

--- scripts/run_inst.sh
+++ scripts/run_inst.sh
@@ -1,34 +1,17 @@
 #! /bin/bash
 
-solve=../bin/resol
+
+
+
 echo "solve K-sat for which value of K ?"
 read K
 echo "how many variables ?"
 read V
-echo "$K" > .result
-echo "$V" >> .result
-echo "no flag" >> .result
-for i in {20..60}; do
-    for j in {1..100}; do
-        (printf "$K\n$V\n$i\n" | ./rand_inst.py) > .tmp.cnf
-        TIMEFORMAT="%3R";ligne=$((time (timeout 10s $solve .tmp.cnf >/dev/null) )2>&1)
-        echo "$ligne">> .result
-    done
-done
-./plot.py < .result
+
+printf "$K\n$V\nno flag" | ./give_data.sh |python plot.py
 
 for flag in -moms -dlis -rand -wl; do
-    echo "$K" > .result
-    echo "$V" >> .result
-    echo "$flag" >> .result
-    for i in {20..60}; do
-        for j in {1..100}; do
-            (printf "$K\n$V\n$i\n" | ./rand_inst.py) > .tmp.cnf
-            TIMEFORMAT="%3R";ligne=$((time (timeout 10s $solve $flag .tmp.cnf >/dev/null) )2>&1)
-            echo "$ligne">> .result
-        done
-    done
-    ./plot.py < .result
+    printf "$K\n$V\n$flag" | ./give_data.sh |python plot.py
 done
 rm .result
 rm .tmp.cnf
