498dfafa260fcb8e79b7bc60cc239782aef0ad0d
scalexm <martin.alex32@hotmail.fr>
2016-04-14 19:04:07 +0200

    Update run_inst.sh

--- scripts/run_inst.sh
+++ scripts/run_inst.sh
@@ -14,9 +14,7 @@ for x in "${paramcl[@]}"; do
     allparam+=("-cl $x")
     allparam+=("-cl $x -forget")
 done;
-#printf "$K\n$V\nno flags" | ./give_data.sh |python plot.py
-
-allparam=( "-cl -moms -forget" )
+printf "$K\n$V\nno flags" | ./give_data.sh |python plot.py
 
 for flag in "${allparam[@]}"; do
     printf "$K\n$V\n$flag" | ./give_data.sh |python plot.py
