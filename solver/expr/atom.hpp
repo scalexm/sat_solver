781c3149e73662c069d3a694030d7edabbbf243f
scalexm <martin.alex32@hotmail.fr>
2016-05-10 14:39:51 +0200

    Add congruence/equality parsing

--- solver/expr/atom.hpp
+++ solver/expr/atom.hpp
@@ -10,12 +10,41 @@
 #define ATOM_HPP
 
 #include <ostream>
+#include <boost/variant.hpp>
+#include <vector>
 
 namespace expr { namespace atom {
     using variable = int;
 
+    struct fun;
+
+    using term = boost::variant<variable, fun>;
+
+    struct fun {
+        char name;
+        std::vector<term> args;
+    };
+
+    inline bool operator ==(const fun & a, const fun & b) {
+        return a.name == b.name && a.args == b.args;
+    }
+
+    inline std::ostream & operator <<(std::ostream & stream, const fun & f) {
+        stream << f.name;
+        if (f.args.size() > 0) {
+            stream << '(';
+            for (auto i = 0; i < f.args.size(); ++i) {
+                if (i != 0)
+                    stream << ", ";
+                stream << f.args[i];
+            }
+            stream << ')';
+        }
+        return stream;
+    }
+
     struct equality {
-        variable left, right;
+        term left, right;
     };
 
     inline bool operator ==(const equality & a, const equality & b) {
--- tests/theory_expr.cpp
+++ tests/theory_expr.cpp
@@ -48,4 +48,44 @@ TEST_CASE("Testing equality expressions parser", "[equality_expr]") {
         s << unwrap(expr::parse_equality("3 = 2 =>(1 =3 X2!= 3)"));
         REQUIRE(s.str() == "(3 = 2 => (1 = 3 X 2 != 3))");
     }
+
+    SECTION("parsing basic congruence expressions") {
+        exp = unwrap(expr::parse_equality("f(1, 2) = a => g(1) = f(3, 4)"));
+        REQUIRE(exp == expr::make_equality(
+            expr::equality_impl {
+                expr::atom::equality {
+                    expr::atom::fun { 'f', { 1, 2 } },
+                    expr::atom::fun { 'a', { } }
+                },
+                expr::atom::equality {
+                    expr::atom::fun { 'g', { 1 } },
+                    expr::atom::fun { 'f', { 3, 4 } }
+                }
+            }
+        ));
+
+        exp = unwrap(expr::parse_equality("f(f(1, 2), c) = g(f(3, 4))"));
+        REQUIRE(exp == expr::make_equality(
+            expr::atom::equality {
+                expr::atom::fun {
+                    'f',
+                    { expr::atom::fun { 'f', { 1, 2 } }, expr::atom::fun { 'c', { } } }
+                },
+                expr::atom::fun {
+                    'g',
+                    { expr::atom::fun { 'f', { 3, 4 } } }
+                }
+            }
+        ));
+    }
+
+    SECTION("testing textual representation") {
+        std::ostringstream s;
+        s << unwrap(expr::parse_equality("f(1,2) !=a => g(1) =f(3, 4)"));
+        REQUIRE(s.str() == "(f(1, 2) != a => g(1) = f(3, 4))");
+
+        s.str("");
+        s << unwrap(expr::parse_equality("a = bX z =f(1, g(3,4)) /\\ 1 = g(2,4)"));
+        REQUIRE(s.str() == "(a = b X (z = f(1, g(3, 4)) /\\ 1 = g(2, 4)))");
+    }
 }
